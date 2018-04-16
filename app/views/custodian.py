24df1e8f11bd6b61377a4496eb7d97bb116d5d54
XCrez <chenhian93@hotmail.com>
2018-04-16 13:26:54 +0800

    Custodian view
    Cashout/Verify premium functions

--- app/views/custodian.py
+++ app/views/custodian.py
@@ -1,19 +1,17 @@
 from flask import redirect, url_for, flash, render_template
-from flask_login import login_required, current_user
+from flask_login import current_user
 
 from app import app
 from ..controllers.login import LoginController
-from ..controllers.custodian import custodian_controller
+from ..controllers.custodian import CustodianController
+
+login_controller = LoginController()
+custodian_controller = CustodianController()
 
 
-@app.route('/custodian-home')
-@login_required
+@app.route('/custodian_home', methods=["GET"])
 def custodian_home():
-    if current_user.role.lower() != "custodian":
-        flash("Not authorized to do such action")
-        return redirect(url_for('index'))
     return render_template('custodian/custodianHome.html', title='Custodian Portal Home', name=current_user.username)
-
 
 @app.route('/custodianViewPendingReimbursement', methods=["GET"])
 def custodianViewPendingReimbursement():
@@ -25,6 +23,30 @@ def custodianViewPendingReimbursement():
     return render_template('custodian/custodianViewPendingReimbursement.html', reimbursements=result,title='View Pending Reimbursement', name=current_user.username)
 
 
+@app.route('/approve-reimbursement/<reimbID>', methods=["GET"])
+def approve_reimbursement(reimbID):
+    if current_user.role != "custodian":
+        flash("Not authorized to do such action")
+        return redirect(url_for('index'))
+    try:
+        custodian_controller.approve_reimbursement(reimbID)
+    except ValueError:
+        flash("Unable to approve reimbursement. Please try again later")
+    return redirect(url_for('custodianViewAllReimbursement'))
+
+
+@app.route('/reject-reimbursement/<reimbID>', methods=["GET"])
+def reject_reimbursement(reimbID):
+    if current_user.role != "custodian":
+        flash("Not authorized to do such action")
+        return redirect(url_for('index'))
+    try:
+        custodian_controller.reject_reimbursement(reimbID)
+    except ValueError:
+        flash("Unable to reject reimbursement. Please try again later")
+    return redirect(url_for('custodianViewAllReimbursement'))
+
+
 @app.route('/custodianViewAllReimbursement')
 def custodianViewAllReimbursement():
     result = []
@@ -34,7 +56,6 @@ def custodianViewAllReimbursement():
         flash("Unable to get reimbursement information. Please try again later")
     return render_template('custodian/custodianViewAllReimbursement.html', reimbursements=result, title='View All Reimbursement', name=current_user.username)
 
-
 @app.route('/custodianViewPendingCashout')
 def custodianViewPendingCashout():
     result = []
@@ -43,6 +64,29 @@ def custodianViewPendingCashout():
     except ValueError:
         flash("Unable to get pending cashout information. Please try again later")
     return render_template('custodian/custodianViewPendingCashout.html', cashouts=result, title='View Pending Cashout', name=current_user.username)
+
+@app.route('/approve-cashout/<cashoutID>', methods=["GET"])
+def approve_cashout(cashoutID):
+    if current_user.role != "custodian":
+        flash("Not authorized to do such action")
+        return redirect(url_for('index'))
+    try:
+        custodian_controller.approve_cashout(cashoutID)
+    except ValueError:
+        flash("Unable to approve cashout. Please try again later")
+    return redirect(url_for('custodianViewAllCashout'))
+
+
+@app.route('/reject-cashout/<cashoutID>', methods=["GET"])
+def reject_cashout(cashoutID):
+    if current_user.role != "custodian":
+        flash("Not authorized to do such action")
+        return redirect(url_for('index'))
+    try:
+        custodian_controller.reject_cashout(cashoutID)
+    except ValueError:
+        flash("Unable to reject cashout. Please try again later")
+    return redirect(url_for('custodianViewAllCashout'))
 
 
 @app.route('/custodianViewAllCashout')
@@ -54,7 +98,33 @@ def custodianViewAllCashout():
         flash("Unable to get cashout information. Please try again later")
     return render_template('custodian/custodianViewAllCashout.html', cashouts=result, title='View Pending Cashout', name=current_user.username)
 
+@app.route('/verify-premium/<premID>', methods=["GET"])
+def verify_premium(premID):
+    if current_user.role != "custodian":
+        flash("Not authorized to do such action")
+        return redirect(url_for('index'))
+    try:
+        custodian_controller.verify_premium(premID)
+    except ValueError:
+        flash("Unable to verify premium. Please try again later")
+    return redirect(url_for('custodianVerifyPremium'))
+
+@app.route('/reject-premium/<premID>', methods=["GET"])
+def reject_premium(premID):
+    if current_user.role != "custodian":
+        flash("Not authorized to do such action")
+        return redirect(url_for('index'))
+    try:
+        custodian_controller.reject_premium(premID)
+    except ValueError:
+        flash("Unable to reject premium. Please try again later")
+    return redirect(url_for('custodianVerifyPremium'))
 
 @app.route('/custodianVerifyPremium')
 def custodianVerifyPremium():
-    return render_template('custodian/custodianVerifyPremium.html', title='Verify Premium', name=current_user.username)
+    result = []
+    try:
+        result = custodian_controller.get_pending_premium()
+    except ValueError:
+        flash("Unable to get pending cashout information. Please try again later")
+    return render_template('custodian/custodianVerifyPremium.html', premiumpayments=result, title='Verify Premium', name=current_user.username)
